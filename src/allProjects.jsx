b7cd81c2352a20be82ae8ed6450c69c1c7f7c085
shashirupa00 <shashishirupa00@gmail.com>
2024-04-25 20:12:19 -0400

    fixed project logo imports

--- src/allProjects.jsx
+++ src/allProjects.jsx
@@ -1,50 +1,59 @@
+import pokedexlogo from "./assets/projectLogos/pokedexlogo.jpeg";
+import worldwiselogo from "./assets/projectLogos/worldwiselogo.jpg";
+import movietrackerlogo from "./assets/projectLogos/movietrackerlogo.webp";
+import reactquizlogo from "./assets/projectLogos/reactquizlogo.png";
+import wildfirelogo from "./assets/projectLogos/wildfirelogo.jpg";
+import bittorrentlogo from "./assets/projectLogos/bittorrentlogo.png";
+import stockslogo from "./assets/projectLogos/stockslogo.jpg";
+import gossip from "./assets/projectLogos/gossip.webp";
+
 export const allProjects = [
   {
     id: 1,
     name: "PokeDex",
-    logo: "./src/assets/projectLogos/pokedexlogo.jpeg",
+    logo: pokedexlogo,
     link: "https://github.com/shashirupa00/pokedex",
   },
   {
     id: 2,
     name: "WorldWise",
-    logo: "./src/assets/projectLogos/worldwiselogo.jpg",
+    logo: worldwiselogo,
     link: "https://github.com/shashirupa00/worldwise",
   },
   {
     id: 3,
     name: "Movie Tracker",
-    logo: "./src/assets/projectLogos/movietrackerlogo.webp",
+    logo: movietrackerlogo,
     link: "https://github.com/shashirupa00/usePopcorn",
   },
   {
     id: 4,
     name: "React Quiz",
-    logo: "./src/assets/projectLogos/reactquizlogo.png",
+    logo: reactquizlogo,
     link: "https://github.com/shashirupa00/reactquiz",
   },
   {
     id: 5,
     name: "California Wildfire Hotspot Prediction",
-    logo: "./src/assets/projectLogos/wildfirelogo.jpg",
+    logo: wildfirelogo,
     link: "https://github.com/Nandini2510/Gossip-Simulator",
   },
   {
     id: 6,
     name: "Bit Torrent Protocol",
-    logo: "./src/assets/projectLogos/bittorrentlogo.png",
+    logo: bittorrentlogo,
     link: "https://github.com/shashirupa00/California-Wildfire-Prediction",
   },
   {
     id: 7,
     name: "Gossip Protocol",
-    logo: "./src/assets/projectLogos/gossip.webp",
+    logo: gossip,
     link: "https://github.com/Nandini2510/Bittorrent-Protocol",
   },
   {
     id: 8,
     name: "Buy and Sell Multiple Stocks",
-    logo: "./src/assets/projectLogos/stockslogo.jpg",
+    logo: stockslogo,
     link: "https://github.com/shashirupa00/Algorithms-Project",
   },
 ];
